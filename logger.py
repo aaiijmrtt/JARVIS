370990d58037cd838248b4f2a8b4bb07838bb336
aaiijmrtt <aaiijmrtt@gmail.com>
2015-12-24 14:00:00 +0500

    dependencies, logs

---
+++ logger.py
@@ -0,0 +1,39 @@
+import logging, logging.config
+
+details = 15
+logging.config.dictConfig({
+	'version': 1,
+	'loggers': {
+		'logger': {
+			'handlers': ['DEBUGLog', 'DETAILLog', 'INFOLog'],
+			'level': 'DEBUG'
+		}
+	},
+	'handlers': {
+		'DEBUGLog': {
+			'class': 'logging.FileHandler',
+			'formatter': 'LogFormat',
+			'filename': 'jarvis.bug',
+			'level': 'DEBUG'
+		},
+		'DETAILLog': {
+			'class': 'logging.FileHandler',
+			'formatter': 'LogFormat',
+			'filename': 'jarvis.log',
+			'level': details
+		},
+		'INFOLog': {
+			'class': 'logging.StreamHandler',
+			'formatter': 'LogFormat',
+			'level': 'INFO'
+		}
+	},
+	'formatters': {
+		'LogFormat': {
+			'format': '[%(asctime)s : %(module)s %(funcName)s : %(levelname)s] %(message)s'
+		}
+	}
+})
+
+logger = logging.getLogger('logger')
+logger.info('LOGGER CREATION')
